17b5ce15f6e76f7bf820741774fe7f152b0cb7aa
Henry Zhang <henry.zhang6@student.tdsb.on.ca>
2025-01-07 10:58:35 -0500

    particles and stuff

--- sketch.js
+++ sketch.js
@@ -1,49 +1,52 @@
 let noiseMap;
 let particles;
+let width,height;
 class Particle{
   constructor(){
-    this.x=random(-180,0);
-    this.y=random(-180,180);
+    this.x=-width/2
+    this.y=random(-height/2,height/2);
   }
   
   display(){
-    fill(100,50,150);
+    fill(255,0,0);
     ellipse(this.x,this.y,3,3);
   }
   
   update(){
-    this.x+=5*cos(noiseMap[int((this.x+180)/10)][int((this.y+180)/10)]);
-    this.y+=5*sin(noiseMap[int((this.x+180)/10)][int((this.y+180)/10)]);
-    if(this.x>180)this.x=0;
-    if(this.y>180)this.y=0;
-    
+    this.x+=2*cos(noiseMap[int((this.x+width/2)/10)][int((this.y+height/2)/10)]);
+    this.y+=2*sin(noiseMap[int((this.x+width/2)/10)][int((this.y+height/2)/10)]);    
   }
 }
 function setup() {
-  createCanvas(400, 400);
-  rectMode(CENTER);
-  angleMode(DEGREES);
-      noiseMap = [];
-  particles = [];
-  for(let i=0;i<100;i++){
-    append(particles,new Particle())
-  }
+    width = 800;
+    height = 500;
+    createCanvas(width, height);
+    rectMode(CENTER);
+    angleMode(DEGREES); 
+    noStroke();
+    noiseMap = [];
+    particles = [];
+    for(let i=0;i<width/2+1;i++){
+        noiseMap[i]=[];
+    }
+    for(let i=0;i<100000;i++){
+      append(particles,new Particle())
+    }
+    background(50,5,40);
+    slopeLines();
 }
 function draw() {
-  background(220);
-  for(let i=0;i<37;i++){
-    noiseMap[i]=[];
-  }
-  display();
+    background(50,5,40,5);
+    display();
 }
 
 function display(){
   push();
   translate(width/2,height/2);
   scale(1,-1);
-  line(-180,0,180,0);
-  line(0,-180,0,180);
-  slopeLines();
+  //line(-width,0,height,0);
+  //line(0,-width,0,height);
+  
   for(let i=0;i<100;i++){
     particles[i].update();
     particles[i].display();
@@ -52,20 +55,21 @@ function display(){
 }
 
 function slopeLines(){
-  for(let i=-180;i<=180;i+=10){
-    for(let j=-180;j<=180;j+=10){
+  for(let i=-width/2;i<width/2;i+=10){
+    for(let j=-height/2;j<height/2;j+=10){
       push();
       translate(i,j);
       rotate(atan(slopeEquation(i,j)));
-      noiseMap[(i+180)/10][(j+180)/10]=atan(slopeEquation(i,j));
-      line(-4,0,4,0);
+      noiseMap[(i+width/2)/10][(j+height/2)/10]=atan(slopeEquation(i,j))+noise(i,j)*100-50;
+      console.log(noise(i,j))
+      //line(-4,0,4,0);
       pop();
     }
   }
 }
 
 function slopeEquation(x,y){
-  let slope = x-y;
+  let slope = 0;
   return slope;
 }
 
