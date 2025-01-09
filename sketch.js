7b0977aa4ed50e5cccb9ffe7171212aaa1a2ef00
Henry Zhang <henry.zhang6@student.tdsb.on.ca>
2025-01-09 11:39:12 -0500

    more experiments

--- sketch.js
+++ sketch.js
@@ -2,26 +2,34 @@ let noiseMap;
 let particles;
 let width,height;
 let particleCount;
+let negative=-1;
 class Particle{
   constructor(){
-    this.x=int(random(-width/100,width/100))*5;
-    this.y=int(random(-height/100,height/100))*5;
     this.negative = int(random(0,2))*2-1
+    this.antiFlip = false;
     this.prevVector=0;
-    this.prevX=this.x;
-    this.prevY=this.y;
     this.finished=false;
-    this.timer=100;
+    this.timeStart=20;
+    this.timer=this.timeStart;
+    this.scaleX=2;
+    this.scaleY=2;
+    this.spawnX();
+    this.spawnY();
   }
   
   display(){
+    if(this.finished){
+        return;
+    }
     //fill(100*this.x/40+100,100*this.y/25+100,this.prevVector+100);
     //fill(255);  
     //stroke(100*this.x/40+100,100*this.y/25+100,this.prevVector+100);
     //stroke(map(this.y,-height/20,height/20,33,255),map(this.y,-height/20,height/20,66,158)+map(-abs(this.x),-2,0,100,150),map(this.y,-height/20,height/20,156,0)) 
-    stroke(0,map(this.y,-height/20,height/20,66,158)+map(-abs(this.x),-3/4,0,100,150),map(-abs(this.y),-height/20,-5,0,50)) 
-
+    //stroke(0,map(this.y,-height/20,height/20,66,158)+map(-abs(this.x),-3/4,0,100,150),map(-abs(this.y),-height/20,-5,0,100)) 
+    //stroke(map(dist(abs(this.x),abs(this.y),0,0),0,20,255,0),map(dist(abs(this.x),abs(this.y),0,0),0,20,100,0),0)
     //console.log(map(this.x,-width/2,width/2,0,255));
+    //stroke(10+map(abs(this.y),0,height/20,0,150),66+map(abs(this.y),0,height/20,0,150),120+map(abs(this.y),0,height/20,0,150))
+    stroke(255);
     strokeWeight(3);
     line(10*this.x,10*this.y,this.prevX*10,this.prevY*10); 
     //ellipse(this.x*10,this.y*10,3,3);
@@ -30,20 +38,22 @@ class Particle{
   update(){
     this.timer--;
     if(this.timer<=0){
-        //this.x=random(-width/20,width/20);
-        //this.y=random(-height/20,height/20);
-        this.x=int(random(-width/100,width/100))*5;
-        this.y=int(random(-height/100,height/100))*5;
-        this.prevX=this.x;
-        this.prevY=this.y;
-        this.timer=100;
-        //this.finished=false;
+        //this.x=random(-width/40,width/40);
+        //this.y=random(-height/30,height/30);
+        //this.x=int(random(-width/100,width/100))*5;
+        //this.y=int(random(-height/100,height/100))*5;
+        this.spawnX();
+        this.spawnY();
+        this.timer=this.timeStart;
         this.finished=false;
         return;
     }
-    if(abs(-1*noiseMap[int((this.x+width/20))][int((this.y+height/20))]-this.prevVector)>abs(noiseMap[int((this.x+width/20))][int((this.y+height/20))]-this.prevVector)){
+    if(this.finished){
+        return;
+    }
+    if(this.antiFlip && abs(-1*noiseMap[int((this.x+width/20))][int((this.y+height/20))]-this.prevVector)>abs(noiseMap[int((this.x+width/20))][int((this.y+height/20))]-this.prevVector)){
         this.negative=1;
-    }else if(abs(-1*noiseMap[int((this.x+width/20))][int((this.y+height/20))]-this.prevVector)<abs(noiseMap[int((this.x+width/20))][int((this.y+height/20))]-this.prevVector)){
+    }else if(this.antiFlip && abs(-1*noiseMap[int((this.x+width/20))][int((this.y+height/20))]-this.prevVector)<abs(noiseMap[int((this.x+width/20))][int((this.y+height/20))]-this.prevVector)){
         this.negative=-1;
     }
     //console.log(noiseMap[int((this.x+width/20))][int((this.y+height/20))])
@@ -56,17 +66,25 @@ class Particle{
     
     if(this.x>width/20 || this.x<-width/20 || this.y<-height/20 || this.y>height/20){
         
-        // this.x=random(-width/20,width/20);
-        // this.y=random(-height/20,height/20);
-        // this.prevX=this.x;
-        // this.prevY=this.y;
+        // this.spawnX();
+        // this.spawnY();
         this.finished=true;
     }
-    
-    
-    //console.log(this.prevVector);
+  }
+
+  spawnX(){
+    this.x=int(random(-width/(this.scaleX*20),width/(20*this.scaleX)))*this.scaleX;
+    this.prevX=this.x;
+  }
+  
+  spawnY(){
+    this.y=int(random(-height/(this.scaleY*20),height/(this.scaleY*20)))*this.scaleY;
+    this.prevY=this.y;
   }
 }
+
+
+
 function setup() {
     width = 800;    
     height = 500;
@@ -99,9 +117,9 @@ function display(){
   //line(0,-width,0,height);
   
   for(let i=0;i<particleCount;i++){
-    if(particles[i].finished){
-        continue;
-    }
+    // if(particles[i].finished){
+    //     continue;
+    // }
     particles[i].update();
     particles[i].display();
     
@@ -124,7 +142,9 @@ function slopeLines(){
 }
 
 function slopeEquation(x,y){
-  let slope = -x*y/2
+  //let slope = 1-x*y/50
+  //let slope = 10*Math.tan(x), timer 20
+  let slope = x*y/50
   return slope;
 }
 
